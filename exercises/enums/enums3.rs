bc768ff1381c65aea3b142c9b2d4aaba06144ca1
jackzj <ybybzj@gmail.com>
2021-09-28 11:34:00 +0800

    update exercises progress

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,12 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Echo(String),
+    Move(Point),
+    ChangeColor((u8, u8, u8)),
+    Quit,
 }
 
 struct Point {
@@ -37,6 +39,12 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match (message) {
+            Message::Move(point) => self.position = point,
+            Message::ChangeColor(color) => self.color = color,
+            Message::Quit => self.quit = true,
+            Message::Echo(str) => println!("{}", &str),
+        }
     }
 }
 
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,11 +7,11 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_apple_price {
-
+fn calculate_apple_price(amount: u32) -> u32 {
+    let price_per_apple = if amount > 40 { 1 } else { 2 };
+    price_per_apple * amount
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
